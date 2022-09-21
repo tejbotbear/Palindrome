a0d7e26fc46e8daa6429ab26d2f5899b76476b47
U-BSD\462750 <462750@SHS-DGCVCS2.beaverton.k12.or.us>
2022-09-21 12:24:05 -0700

    Need to add reversing and comparison

--- pdrome.cpp
+++ pdrome.cpp
@@ -5,18 +5,33 @@ using namespace std;
 int main() {
 
   int Length;
-  
+  char Temp[100];
   char Input[100];
-  cin.getline(Input, 100, '\n');
+  cin.getline(Input, 100/*, '\n'*/);
   Length = strlen(Input);
-  //cout << Input << endl;
+
 
   for (int i = 0; i < Length; i++) {
+    Temp[i] = '\0';
+  }
 
+  
+  //  /*
+  int t = 0;
+  for (int i = 0; i < Length; i++) {
     if (isalpha(Input[i])) {
-
-	cout << "hello" << endl;
-      
+      Temp[t] = Input[i];
+      t++;
+      //cout << Input << endl;
+      //cout << Temp << endl;
+    }
+    else if (Input[i] == ' ') {
+      //cout << "there would be a space here" << endl;
+    }
+    else {
+      //cout << "else has been hit" << endl;  
     }
   }
-}
+  cout << Temp << endl;
+  }
+
