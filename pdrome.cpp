05d20ba897e2a4c64eec888611fd62fe399c4e44
U-BSD\462750 <462750@SHS-DGCVCS2.beaverton.k12.or.us>
2022-09-23 12:18:48 -0700

    Everything works, can check for palindrome and tell user.

--- pdrome.cpp
+++ pdrome.cpp
@@ -1,65 +1,59 @@
+/*
+Tej Hiremath
+This program checks a users input to see if it is a palindrome when only letters are considered.
+9/23/2022
+*/
 #include <iostream>
 #include <cstring>
 using namespace std;
 
 int main() {
 
+  // Initialize all necessary Variables
   int Length;
+  int PalindromeChecker;
   int TempLength;
-  int Remainder;
-  int Division;
-  int two = 2;
   char checker1;
   char checker2;
-  char Temp[100];
-  char Temp2[100];
-  char Input[100];
-  char Reverse[100];
-  cin.getline(Input, 100/*, '\n'*/);
+  char Temp[80];
+  char Input[80];
+  // Get input and set length
+  cin.getline(Input, 80/*, '\n'*/);
   Length = strlen(Input);
 
-
+  // Clear memory for the length of the temporary array
   for (int i = 0; i < Length; i++) {
     Temp[i] = '\0';
   }
 
   int t = 0;
+  // Get length of filtered text
   for (int i = 0; i < Length; i++) {
     if (isalpha(Input[i])) {
       Temp[t] = Input[i];
       t++;
-      //cout << Input << endl;
-      //cout << Temp << endl;
-    }
-    else if (Input[i] == ' ') {
-      //cout << "there would be a space here" << endl;
     }
     else {
-      //cout << "else has been hit" << endl;  
+      //keep going  
     }
   }
+  // Check to see if it is a palindrome
   TempLength = strlen(Temp);
-  cout << TempLength;
-  Remainder = TempLength % two;
-  if (Remainder != 0); {
-    TempLength = TempLength - 1;
-    cout << TempLength;
+  for (int r = TempLength - 1, i = 0; r >= 0; r--, i++) {
+    checker1 = Temp[r];
+    checker2 = Temp[i];
+    if (checker1 != checker2) {
+      r = 0; // Confirmed not a palindrome, stop checking.
+      PalindromeChecker = 0;
+    }
+    else {
+      PalindromeChecker = 1;
+    }
+  }
+  if (PalindromeChecker == 1) {
+    cout << "Palindrome." << endl;
   }
   else {
-    //Division = TempLength / two;
-
-    Temp2 = Temp;
-    for (int r = Length; r >= 0; r--) {
-      for (int i = 0; i < Length; i++) {
-	checker1 = getchar(Temp[r]);
-	checker2 = getchar(Temp2[i]);
-	if (checker1 == checker2) {
-	  // do nothing -> Keep going
-	}
-	else {
-	  cout << "error" << endl;
-	}
-      }
-    }
+   cout << "Not a palindrome." << endl;
   }
 }
