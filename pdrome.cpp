f4469396d563fa3f05292912f660f19ba7c997ab
tej <tejbotbear@gmail.com>
2022-09-23 05:10:07 +0100

    Need to fix Palindrome reverse searching and  comparison

--- pdrome.cpp
+++ pdrome.cpp
@@ -5,8 +5,16 @@ using namespace std;
 int main() {
 
   int Length;
+  int TempLength;
+  int Remainder;
+  int Division;
+  int two = 2;
+  char checker1;
+  char checker2;
   char Temp[100];
+  char Temp2[100];
   char Input[100];
+  char Reverse[100];
   cin.getline(Input, 100/*, '\n'*/);
   Length = strlen(Input);
 
@@ -15,8 +23,6 @@ int main() {
     Temp[i] = '\0';
   }
 
-  
-  //  /*
   int t = 0;
   for (int i = 0; i < Length; i++) {
     if (isalpha(Input[i])) {
@@ -32,6 +38,28 @@ int main() {
       //cout << "else has been hit" << endl;  
     }
   }
-  cout << Temp << endl;
+  TempLength = strlen(Temp);
+  cout << TempLength;
+  Remainder = TempLength % two;
+  if (Remainder != 0); {
+    TempLength = TempLength - 1;
+    cout << TempLength;
   }
+  else {
+    //Division = TempLength / two;
 
+    Temp2 = Temp;
+    for (int r = Length; r >= 0; r--) {
+      for (int i = 0; i < Length; i++) {
+	checker1 = getchar(Temp[r]);
+	checker2 = getchar(Temp2[i]);
+	if (checker1 == checker2) {
+	  // do nothing -> Keep going
+	}
+	else {
+	  cout << "error" << endl;
+	}
+      }
+    }
+  }
+}
